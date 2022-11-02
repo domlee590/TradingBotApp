c04bd0c130deec624d3008017b916a3788d2fd33
eh2889 <eh2889@columbia.edu>
2022-11-02 18:46:39 -0400

    Updating Cucumber

--- features/step_definitions/bot_steps.rb
+++ features/step_definitions/bot_steps.rb
@@ -1,8 +1,11 @@
-And(/^I click "([^"]*)"$/) do |arg|
-  pending
-end
-
 When('I go to the TradingBotApp home page') do
   visit '/'
 end
 
+And(/^I press "([^"]*)"$/) do |arg|
+  click_link(arg)
+end
+
+Then /^(?:|I )go to (.+)$/ do |page_name|
+  visit path_to(page_name)
+end
