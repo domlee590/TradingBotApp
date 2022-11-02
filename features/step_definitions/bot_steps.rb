7bf685d08fdfce28a34b775e92ba111270f5d34d
domlee590 <domlee590@gmail.com>
2022-11-02 19:09:58 -0400

    Change titles and matching page cucumber

--- features/step_definitions/bot_steps.rb
+++ features/step_definitions/bot_steps.rb
@@ -6,3 +6,7 @@ When('I go to the TradingBotApp home page') do
   visit '/'
 end
 
+#expect to be on the home page
+Then('I should be on the page {string}') do |string|
+  expect(page).to have_title(string)
+end
