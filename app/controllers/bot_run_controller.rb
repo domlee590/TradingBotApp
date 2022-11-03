dd92e039eb41ce9ab2d476e2654f89aa5c4b2516
eh2889 <eh2889@columbia.edu>
2022-11-02 23:19:05 -0400

    update

--- app/controllers/bot_run_controller.rb
+++ app/controllers/bot_run_controller.rb
@@ -1,9 +1,9 @@
 class BotRunController < ApplicationController
-  def run
+  def show
     name = "bob"
     ma1 = 1
     ma2 = 48
     short = "yes"
-    @pythonbotrun = 'python3 lib/assets/paperTrading.py "#{name} #{ma1} #{ma2} #{short}"'
+    @pythonbotrun = 'python lib/assets/paperTrading.py "#{name} #{ma1} #{ma2} #{short}"'
   end
 end
