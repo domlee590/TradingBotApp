2e50293d8c097e32e699a24f71c2341fbe8ea4bf
eh2889 <eh2889@columbia.edu>
2022-11-15 19:14:54 -0500

    starting login

--- app/controllers/bot_run_controller.rb
+++
@@ -1,9 +0,0 @@
-class BotRunController < ApplicationController
-  def show
-    name = "bob"
-    ma1 = 1
-    ma2 = 48
-    short = "yes"
-    @pythonbotrun = `python3 lib/assets/paperTrading.py "#{name} #{ma1} #{ma2} #{short}"`
-  end
-end
