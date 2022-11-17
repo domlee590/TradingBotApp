d18b94ed2fea81c3cc451b3c7c32935287c4e625
eh2889 <eh2889@columbia.edu>
2022-11-16 21:03:36 -0500

    Style changes

--- spec/controllers/bots_controller_spec.rb
+++ spec/controllers/bots_controller_spec.rb
@@ -10,30 +10,14 @@ describe BotsController do
 
   describe 'POST #create' do
     it 'creates a new bot' do
-      expect {post :create, params: {bot: {name: "Bobby", movingAverage1: 9, movingAverage2: 60, short: "yes"}}
+      expect {post :create, params: {bot: {name: "Bobby", ema: 9, bb: 60,  short: "true"}}
       }.to change { Bot.count }.by(1)
-    end
-  end
-
-  describe 'GET #edit' do
-    it 'should render the edit template' do
-      bot = Bot.create(name: "Bobby", movingAverage1: 9, movingAverage2: 60, short: "yes")
-      get :edit, params: {id: bot.id}
-      expect(response).to render_template('edit')
-    end
-  end
-
-  describe 'PUT #update' do
-    it 'should update the bot' do
-      bot = Bot.create(name: "Bobby", movingAverage1: 9, movingAverage2: 60, short: "yes")
-      put :update, params: {id: bot.id, bot: {name: "Bob"}}
-      expect(bot.reload.name).to eq("Bob")
     end
   end
 
   describe 'show' do
     it 'should render the show template' do
-      bot = Bot.create(name: "Bobby", movingAverage1: 9, movingAverage2: 60, short: "yes")
+      bot = Bot.create(name: "Bobby", ema: 9, bb: 60,  short: "true")
       get :show, params: {id: bot.id}
       expect(response).to render_template('show')
     end
